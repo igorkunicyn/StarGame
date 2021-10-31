6ebe9da47dc4ec1f20263335e34cf05e5b152b33
Igor Kunicyn <igor_kunicyn@inbox.ru>
2021-10-31 15:34:05 +0300

    lesson_6hw

--- core/src/gb/ru/sprite/EnemyShip.java
+++ core/src/gb/ru/sprite/EnemyShip.java
@@ -10,6 +10,16 @@ import gb.ru.pool.BulletPool;
 
 public class EnemyShip extends Ship {
 
+    private static final float RELOAD_INTERVAL_SMALL_SHIP = 0.4f;
+    private static final float RELOAD_INTERVAL_MEDIUM_SHIP = 0.5f;
+    private static final float RELOAD_INTERVAL_BIG_SHIP = 0.6f;
+
+    private final Vector2 enemySmallV = new Vector2(0f, -0.2f);
+    private final Vector2 enemyMediumV = new Vector2(0f, -0.03f);
+    private final Vector2 enemyBigV = new Vector2(0f, -0.005f);
+
+    private float type;
+
     public EnemyShip(BulletPool bulletPool, Rect worldBounds, Sound bulletSound) {
         this.bulletPool = bulletPool;
         this.worldBounds = worldBounds;
@@ -23,6 +33,21 @@ public class EnemyShip extends Ship {
     @Override
     public void update(float delta) {
         super.update(delta);
+        if (getTop() < worldBounds.getTop()) {
+            if (type < 0.5f && type >= 0f) {
+                v.set(enemySmallV);
+                reloadInterval = RELOAD_INTERVAL_SMALL_SHIP;
+            }
+            if (type < 0.8f && type >= 0.5f) {
+                v.set(enemyMediumV);
+                reloadInterval = RELOAD_INTERVAL_MEDIUM_SHIP;
+            }
+            if (type < 1f && type >= 0.8f) {
+                v.set(enemyBigV);
+                reloadInterval = RELOAD_INTERVAL_BIG_SHIP;
+            }
+        }
+
         if (getBottom() < worldBounds.getBottom()) {
             destroy();
         }
@@ -37,7 +62,8 @@ public class EnemyShip extends Ship {
             int damage,
             int hp,
             float reloadInterval,
-            float height
+            float height,
+            float type
     ) {
         this.regions = regions;
         this.v.set(v);
@@ -48,5 +74,6 @@ public class EnemyShip extends Ship {
         this.hp = hp;
         this.reloadInterval = reloadInterval;
         setHeightProportion(height);
+        this.type = type;
     }
 }
